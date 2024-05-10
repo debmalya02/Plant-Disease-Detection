2c482653fb1d2d511cf985a1d96624d6ca8aba3c
Debmalya dash <98879060+debmalya02@users.noreply.github.com>
2024-05-10 21:15:31 +0530

    added:streamlit pages

---
+++ pages/1_About Us.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.header("About")
+st.markdown("""
+                #### About Dataset
+                This dataset is recreated using offline augmentation from the original dataset.The original dataset can be found on this github repo.
+                This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
+                A new directory containing 33 test images is created later for prediction purpose.
+                #### Content
+                1. train (70295 images)
+                2. test (33 images)
+                3. validation (17572 images)
+
+                """)
